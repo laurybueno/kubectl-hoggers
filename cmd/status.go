830546df43f248c090bf7e0e6cb6d68f5f99ac73
Laury Bueno <laury.bueno@gmail.com>
2020-07-26 15:23:41 -0300

    Add help texts to current commands

--- cmd/status.go
+++ cmd/status.go
@@ -69,13 +69,9 @@ const apiCheckInterval = 10
 // statusCmd represents the status command
 var statusCmd = &cobra.Command{
 	Use:   "status",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Watch pods consuming most resources",
+	Long: `List pods consuming most CPU resources along with its corresponding nodes.
+Refreshes every 10 seconds.`,
 	Run: run,
 }
 
@@ -247,16 +243,6 @@ func init() {
 
 	clientset = getClientSet()
 	metricsClientset = getMetricsClientset()
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// statusCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// statusCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func getClientSet() *kubernetes.Clientset {
